f8a7ebe8f008e747e74a92b7e5a46a6be6a4290b
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-05-01 16:56:10 +0000

    saved unit 5-05 code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Kyle Matthew Magnaye
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kyle Matthew Magnaye
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+function checkNumber() {
+  let lengthA = parseInt(document.getElementById("length-a").value)
+  let lengthB = parseInt(document.getElementById("length-b").value)
+  let lengthC = parseInt(document.getElementById("length-c").value)
+  let message = ""
+
+  if (
+    lengthA + lengthB <= lengthC ||
+    lengthA + lengthC <= lengthB ||
+    lengthB + lengthC <= lengthA
+  ) {
+    message = "It doesn't form a triangle"
+  } else if (lengthA == lengthB && lengthA == lengthC) {
+    message = "It's an Equilateral Triangle"
+  } else if (lengthA == lengthB || lengthA == lengthC || lengthB == lengthC) {
+    message = "It's an Isosceles Triangle"
+  } else {
+    message = "It's a Scalene Triangle"
+  }
+
+  document.getElementById("message").innerText = message
+}
